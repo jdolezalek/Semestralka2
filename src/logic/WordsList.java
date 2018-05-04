6e4b58701409490f8454c3c02afb001e4a41ca13
stepankalensky <skalensky@gmail.com>
2018-05-04 16:52:18 +0200

    Komentáře třídy WordsList

--- src/logic/WordsList.java
+++ src/logic/WordsList.java
@@ -1,6 +1,6 @@
 /**
- *  T��da WordsList -
- *@author     Dole��lek Jan, Kalensk� �t�p�n, �orf Filip
+ *  Třída WordsList - reprezentuje slova a jejich významy.
+ *@author     Doležálek Jan, Kalenský Štěpán, Šorf Filip
  *@version    1.0      
  *@created    LS 2017/2018
  */
@@ -21,16 +21,20 @@ import java.util.Observable;
 public class WordsList extends Observable {
 
     private HashMap<String, Word> stringWordsMap;
-
-
+/**
+ * Metoda vytváří mapu slov a významů. Přidává i významy ke slovům z JSONu.
+ * @param stringWordsMap - mapa slov a významů
+ */
     public WordsList(HashMap<String, Word> stringWordsMap) {
         this.stringWordsMap = stringWordsMap;
 
-        //add word meanings into stringWordsMap from json, if any exists
+        //přidává významy ke slovům do stringWordsMap z JSONu, pakliže některé existují.
         this.setWordsFromJSON();
 
     }
-
+/**
+ * Metoda, která přidává významy slov z JSONu do StringWordMapy.
+ */
     private void setWordsFromJSON() {
 
         JSONParser parser = new JSONParser();
@@ -61,34 +65,57 @@ public class WordsList extends Observable {
         }
 
     }
-
+/**
+ * Metoda vrací mapu slov a významů
+ * @return stringWordsmap - mapa slov a významů.
+ */
     public HashMap<String, Word> getStringWordsMap() {
         return this.stringWordsMap;
     }
-
+/**
+ * Metoda přidává význam ke slovu.
+ * @param word - slovo vyskytující se v textu.
+ * @param meaning - význam slova
+ */
     public void addMeaningToWord(String word, String meaning) {
         stringWordsMap.get(word).addMeaningToWord(meaning);
 
         notifyController();
     }
-
+/**
+ * Metoda vrací slovo nebo null.
+ * @param word - slovo z textu
+ * @return word; null - vrátí buď slovo z mapy nebo null
+ */
     public Word getWord(String word) {
         return stringWordsMap.getOrDefault(word, null);
 
     }
-
+/**¨
+ * Metoda nastaví index významu ke slovu v listu.
+ * @param word - slovo z textu
+ * @param meaning - význam
+ */
     public void setPrimaryMeaningIndex(String word, int meaning) {
         if (this.stringWordsMap.get(word) != null) {
             this.stringWordsMap.get(word).setPrimaryMeaningIndex(meaning);
         }
     }
-
+/**
+ * Metoda nastaví význam slovu.
+ * @param word - slovo z textu
+ * @param meaning - význam
+ */
     public void setPrimaryMeaning(String word, String meaning) {
         if (this.stringWordsMap.get(word) != null) {
             this.stringWordsMap.get(word).setPrimaryMeaning(meaning);
         }
     }
-
+/**
+ * Metoda vrací list významů přiřazených ke slovu.
+ * @param word - slovo z textu
+ * @return ArrayList - list významů přiřazených ke slovu.
+ */
     public ArrayList<String> getWordMeanings(String word) {
         if (this.stringWordsMap.get(word) != null) {
             return this.stringWordsMap.get(word).getMeanings();
@@ -96,7 +123,9 @@ public class WordsList extends Observable {
             return new ArrayList<>();
         }
     }
-
+/**
+ * Metoda upozorňující Controller.
+ */
     private void notifyController() {
         this.setChanged();
         this.notifyObservers();
